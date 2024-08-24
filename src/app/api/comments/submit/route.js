0979e00b15648fbe868a32e02333cc5a06472b64
Fariha-Rana <fariharana.eth@gmail.com>
2024-08-24 23:21:43 +0530

    added webhook api

--- src/app/api/comments/submit/route.js
+++ src/app/api/comments/submit/route.js
@@ -22,6 +22,103 @@ export async function POST(req) {
     });
   }
 
+  await fetch(
+    "https://hooks.slack.com/services/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",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        blocks: [
+          {
+            type: "header",
+            text: {
+              type: "plain_text",
+              text: "New comment waiting for approval! :meow_party:",
+              emoji: true,
+            },
+          },
+          {
+            type: "section",
+            fields: [
+              {
+                type: "mrkdwn",
+                text: `*Blog post:*\nhttp://localhost:3000/articles/${uid}`,
+              },
+              {
+                type: "mrkdwn",
+                text: `*Comment ID:*\n<https://supabase.com/dashboard/project/kmimxhcpximbajtshrve/editor/28564|${post_id}>`,
+              },
+            ],
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `*Comment:*\n${comment}`,
+            },
+          },
+          {
+            type: "context",
+            elements: [
+              {
+                type: "mrkdwn",
+                text: `*Submitted by:* ${nickname} (<mailto:${email}|${email}>)`,
+              },
+            ],
+          },
+          {
+            type: "divider",
+          },
+          {
+            type: "actions",
+            elements: [
+              {
+                type: "button",
+                text: {
+                  type: "plain_text",
+                  emoji: true,
+                  text: "Approve",
+                },
+                style: "primary",
+                action_id: "approve_comment",
+                value: data[0].id,
+              },
+              {
+                type: "button",
+                text: {
+                  type: "plain_text",
+                  emoji: true,
+                  text: "Delete",
+                },
+                style: "danger",
+                action_id: "delete_comment",
+                value: data[0].id,
+                confirm: {
+                  title: {
+                    type: "plain_text",
+                    text: "Are you sure?",
+                  },
+                  text: {
+                    type: "mrkdwn",
+                    text: "This will delete the comment permanently.",
+                  },
+                  confirm: {
+                    type: "plain_text",
+                    text: "Delete",
+                  },
+                  deny: {
+                    type: "plain_text",
+                    text: "Cancel",
+                  },
+                  style: "danger",
+                },
+              },
+            ],
+          },
+        ],
+      }),
+    }
+  );
+
   return new NextResponse(JSON.stringify({ success: true }), {
     status: 200,
   });
